d4c47ba40a56d205aa740fb841a2d798b96a1f26
AaleXxx <alexander.kittelmann@gmx.de>
2016-08-25 21:12:29 +0200

    .

---
+++ customization/execute.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+JENKINS_SH=/usr/local/bin/jenkins.sh
+JENKINS_HOME=/var/jenkins_home
+JENKINS_CLI=$JENKINS_HOME/war/WEB-INF/jenkins-cli.jar
+
+function wait_for_jenkins() {
+  while [[ $(curl -s -w "%{http_code}" http://localhost:5432 -o /dev/null) != "200" ]]; do
+     sleep 5
+  done
+}
+
+echo "=> Starting Jenkins"
+cd /var/jenkins_home/
+java -jar jenkins.war --httpPort=5432 --ajp13Port=-1 > /dev/null &
+
+echo "=> Waiting for the server to boot"
+wait_for_jenkins
+
+echo "=> Executing the commands"
+java -jar $JENKINS_CLI -s 'http://localhost:5432' install-plugin git
+java -jar $JENKINS_CLI -s 'http://localhost:5432' install-plugin workflow-aggregator
+java -jar $JENKINS_CLI -s 'http://localhost:5432' install-plugin pipeline-stage-view
+java -jar $JENKINS_CLI -s 'http://localhost:5432' install-plugin docker-plugin
+java -jar $JENKINS_CLI -s 'http://localhost:5432' install-plugin docker-workflow
+
+echo "=> Restart Jenkins"
+java -jar $JENKINS_CLI -s 'http://localhost:5432' safe-shutdown
+java -jar jenkins.war --httpPort=5432 --ajp13Port=-1
